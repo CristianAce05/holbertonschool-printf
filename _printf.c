f80c3016e947d4c9149036f74b74e9fa41b2f326
Cristian Acevedo <acecristian35@gmail.com>
2025-11-19 15:15:01 -0500

    Functionzz

--- _printf.c
+++ _printf.c
@@ -1,84 +1,42 @@
 #include "main.h"
 
 /**
- * get_spec_func - Selects the correct printing function
- * @c: The format specifier
- *
- * Return: Pointer to function, or NULL if unknown
- */
-static int (*get_spec_func(char c))(va_list)
-{
-	int i;
-	spec_t specs[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_int},
-		{'i', print_int},
-		{'u', print_unsigned},
-		{'o', print_octal},
-		{'x', print_hex_lower},
-		{'X', print_hex_upper},
-		{'p', print_pointer},
-		{'\0', NULL}
-	};
-
-	for (i = 0; specs[i].symbol != '\0'; i++)
-	{
-		if (specs[i].symbol == c)
-			return (specs[i].func);
-	}
-	return (NULL);
-}
-
-/**
- * _printf - Custom printf function
- * @format: The format string containing directives
- *
- * Return: Number of characters printed,
- *         or -1 if format is NULL or an error occurs
+ * _printf - simplified printf function
+ * @format: format string
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, count = 0;
-	int (*func)(va_list);
+	int i, j, count = 0;
+	fmt_t specifiers[] = {
+		{'c', print_char}, {'s', print_string},
+		{'%', print_percent}, {'d', print_int},
+		{'i', print_int}, {'\0', NULL}};
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 
 	va_start(args, format);
-
-	while (format[i])
+	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == '\0')
-			{
-				va_end(args);
+			if (!format[i])
 				return (-1);
-			}
-
-			func = get_spec_func(format[i]);
-			if (func != NULL)
-				count += func(args);
-			else
-			{
-				/* print unknown specifier exactly like printf: "%r" → "%r" */
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
+			for (j = 0; specifiers[j].spec; j++)
+				if (format[i] == specifiers[j].spec)
+				{
+					count += specifiers[j].func(args);
+					break;
+				}
+			if (!specifiers[j].spec)
+				count += _putchar('%'), count += _putchar(format[i]);
 		}
 		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		i++;
+			count += _putchar(format[i]);
 	}
-
 	va_end(args);
 	return (count);
 }
--- main.h
+++ main.h
@@ -4,38 +4,26 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
 
 /**
- * struct specifier - Struct to map format specifier to function
- * @symbol: The format specifier (e.g. 'c', 's', 'd')
- * @func: Pointer to the function that handles printing
+ * struct fmt - Struct for format specifiers
+ * @spec: The format specifier character
+ * @func: The function associated with the specifier
  */
-typedef struct specifier
+typedef struct fmt
 {
-	char symbol;
+	char spec;
 	int (*func)(va_list);
-} spec_t;
+} fmt_t;
 
-/* main printf function */
 int _printf(const char *format, ...);
-
-/* handlers for each specifier */
+int _putchar(char c);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_int(va_list args);
-int print_unsigned(va_list args);
-int print_octal(va_list args);
-int print_hex_lower(va_list args);
-int print_hex_upper(va_list args);
-int print_pointer(va_list args);
 
-/* helper functions */
-int _putchar(char c);
-int print_number(long n);
-int print_unsigned_number(unsigned long n);
-int print_hex(unsigned long n, int uppercase);
-int print_octal_number(unsigned long n);
-
-#endif /* MAIN_H */
-
+#endif
